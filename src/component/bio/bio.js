ee2035b7b22a6513929aa4719035a42897da27b0
muhammed youssef <muhammedelnazer@gmail.com>
2021-11-30 21:46:55 +0200

    WebPack Task

---
+++ src/component/bio/bio.js
@@ -0,0 +1,7 @@
+import './style.scss';
+
+    const element = document.createElement("div");
+    element.innerHTML = "Muhammed Youssef";
+    element.classList.add('myDiv')
+
+document.body.appendChild(element);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style.css"
+
+import header from "./component/header/header"
+
+import bio from "./component/bio/bio"
